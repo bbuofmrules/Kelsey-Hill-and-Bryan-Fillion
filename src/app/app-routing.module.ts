b5d0462db768cb537c35b83b56249926814133d9
Bryan Fillion <bryan.fillion@refinish-eproducts.net>
2020-02-15 13:00:41 -0500

    adding primeng primeflex and primeicons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './404/404.component';
+import { ButtonModule } from 'primeng/button';
 
 
 const routes: Routes = [
-  {path:'', redirectTo:'home', pathMatch:'full'},
-  {path:'home', component: HomepageComponent},
-  {path: 'about', component:AboutComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomepageComponent },
+  { path: 'about', component: AboutComponent },
   { path: '404', component: PageNotFoundComponent },
   { path: '**', redirectTo: '404', pathMatch: 'full' }
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+    ButtonModule
+  ],
+  exports: [RouterModule, ButtonModule]
 })
 export class AppRoutingModule { }
