868744e23144c92f7b311882b21c2e100c87eaa6
Bryan Fillion <bryan.fillion@refinish-eproducts.net>
2020-02-15 02:37:37 -0500

    [app-routing.module.ts] adding a 404 page!

---
+++ src/app/404/404.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './404.component.html'
+})
+export class PageNotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './404/404.component';
 
 
 const routes: Routes = [
-  {path:'', component: HomepageComponent},
-  {path: 'about', component:AboutComponent}
-  //{ path: '404', component: PageNotFoundComponent },
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'home', component: HomepageComponent},
+  {path: 'about', component:AboutComponent},
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '404', pathMatch: 'full' }
 
 ];
 
